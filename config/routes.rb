23c76948ae31aa3aa38cf34bbfa294cbcc4d6f0b
Ingrid Molloy <ingrid.molloy@shopify.com>
2018-06-23 00:21:04 -0700

    added shots and user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :shots
+
+  devise_for :users, controllers: { registrations: 'registrations' }
+
+  # root to: "home#index"
+  root 'shots#index'
+
 end
